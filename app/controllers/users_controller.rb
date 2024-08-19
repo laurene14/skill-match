b618a363f6dcda69daf4c5f38c277912eaa761b5
Severine CELIGNY <severine.celigny@gmail.com>
2024-08-19 16:34:13 +0200

    Cloudinary integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
   def show
 
   end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:email, :password, :username, :status, :location, :bio, :distance_preference, :photos [])
+  end
 end
