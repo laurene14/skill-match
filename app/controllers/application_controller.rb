33f3828bd2f8ab5311cd798ea670f741a97b8be0
Xinhe YU <aspasia.yu@gmail.com>
2024-08-20 12:53:45 +0200

    pundit debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -15,8 +14,8 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   # Pundit: allow-list approach
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: %i[index], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: %i[index], unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -28,6 +27,9 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    result = devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    logger.debug "skip_pundit? result: #{result}"
+    result
   end
+
 end
