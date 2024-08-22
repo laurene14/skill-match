e41add4cce518b868cd211f14b1de3bb49e319f2
Severine CELIGNY <severine.celigny@gmail.com>
2024-08-22 16:24:15 +0200

    Registration step3 - transform skills into tags

--- app/controllers/user_profile/form_skills_controller.rb
+++ app/controllers/user_profile/form_skills_controller.rb
@@ -1,17 +1,24 @@
 module UserProfile
   class FormSkillsController < ApplicationController
     def new
-      @user_skill = UserSkill.new
-      authorize @user_skill
+      @form_skill = FormSkill.new
+      authorize @form_skill
       @categories = Category.where(id: params[:categories][:name])
       # @skills = @categories.skills
       # raise
     end
 
     def create
-      authorize @user_skill_ids
-      @user_skill_ids = UserSkill.new(form_skill_params)
-      raise
+      params[:user_profile_form_skill][:name].delete('')
+      @form_skill_ids = params[:user_profile_form_skill][:name]
+      @form_skill_ids.each do |skill_id|
+        UserSkill.create(
+          user: current_user,
+          skill_id: skill_id,
+          wanted: false
+        )
+      end
+      redirect_to 
     end
 
     private
