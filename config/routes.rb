3a3552689518a72a737c45d34282c49a33f04cc0
Adil Sari <sari.helfi@gmail.com>
2024-08-22 16:00:11 +0200

    autocomplete and new front for description page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   namespace :user_profile do
     resources :user_descriptions, only: %i[new create]
     resources :user_skill_categories, only: %i[new]
-    resources :user_skills, only: %i[new create]
+    resources :form_skills, only: %i[new create]
   end
 end
