48355fcf1b110830ca6d6f9a5c59fddf57be79d1
Severine CELIGNY <severine.celigny@gmail.com>
2024-08-29 09:15:07 +0200

    Display skills of a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,15 @@ class CategoriesController < ApplicationController
     @users = policy_scope(User)
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    authorize @category
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_id)
+  end
 end
